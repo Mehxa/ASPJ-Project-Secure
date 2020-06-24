218034898c4607fe9cb1592335722fe25128b035
Mehxa <59093284+Mehxa@users.noreply.github.com>
2020-06-24 11:32:41 +0800

    Db updated

--- secure_database.sql
+++ secure_database.sql
@@ -18,6 +18,32 @@ USE `secureblogdb`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin` (
+  `AdminID` tinyint(3) NOT NULL AUTO_INCREMENT,
+  `UserID` tinyint(3) NOT NULL,
+  PRIMARY KEY (`AdminID`),
+  UNIQUE KEY `AdminID_UNIQUE` (`AdminID`),
+  KEY `fk_admin_UserID_idx` (`UserID`),
+  CONSTRAINT `fk_admin_UserID` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `comment`
 --
 
@@ -28,7 +54,6 @@ CREATE TABLE `comment` (
   `CommentID` smallint(5) NOT NULL AUTO_INCREMENT,
   `PostID` smallint(5) NOT NULL,
   `UserID` tinyint(3) NOT NULL,
-  `RepliedID` smallint(5) DEFAULT NULL,
   `DatetimePosted` datetime NOT NULL,
   `Content` mediumtext NOT NULL,
   `Upvotes` mediumint(8) NOT NULL,
@@ -37,10 +62,8 @@ CREATE TABLE `comment` (
   UNIQUE KEY `DatetimePosted_UNIQUE` (`DatetimePosted`),
   UNIQUE KEY `CommentID_UNIQUE` (`CommentID`),
   KEY `fk_comment_UserID_idx` (`UserID`),
-  KEY `fk_comment_RepliedID_idx` (`RepliedID`),
   KEY `fk_comment_PostID_idx` (`PostID`),
   CONSTRAINT `fk_comment_PostID` FOREIGN KEY (`PostID`) REFERENCES `post` (`PostID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_comment_RepliedID` FOREIGN KEY (`RepliedID`) REFERENCES `comment` (`CommentID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_comment_UserID` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -52,6 +75,33 @@ CREATE TABLE `comment` (
 LOCK TABLES `comment` WRITE;
 /*!40000 ALTER TABLE `comment` DISABLE KEYS */;
 /*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comment_votes`
+--
+
+DROP TABLE IF EXISTS `comment_votes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comment_votes` (
+  `UserID` tinyint(3) NOT NULL,
+  `CommentID` smallint(5) NOT NULL,
+  `Vote` tinyint(4) NOT NULL,
+  PRIMARY KEY (`UserID`,`CommentID`),
+  KEY `fk_comment_votes_CommentID_idx` (`CommentID`),
+  CONSTRAINT `fk_coment_votes_UserID` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comment_votes_CommentID` FOREIGN KEY (`CommentID`) REFERENCES `comment` (`CommentID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment_votes`
+--
+
+LOCK TABLES `comment_votes` WRITE;
+/*!40000 ALTER TABLE `comment_votes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment_votes` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -120,6 +170,63 @@ LOCK TABLES `post` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `post_votes`
+--
+
+DROP TABLE IF EXISTS `post_votes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post_votes` (
+  `UserID` tinyint(3) NOT NULL,
+  `PostID` smallint(5) NOT NULL,
+  `Vote` tinyint(4) NOT NULL,
+  PRIMARY KEY (`UserID`,`PostID`),
+  KEY `fk_post_votes_PostID_idx` (`PostID`),
+  CONSTRAINT `fk_post_votes_PostID` FOREIGN KEY (`PostID`) REFERENCES `post` (`PostID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_post_votes_UserID` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post_votes`
+--
+
+LOCK TABLES `post_votes` WRITE;
+/*!40000 ALTER TABLE `post_votes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post_votes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reply`
+--
+
+DROP TABLE IF EXISTS `reply`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reply` (
+  `ReplyID` smallint(5) NOT NULL AUTO_INCREMENT,
+  `CommentID` smallint(5) NOT NULL,
+  `UserID` tinyint(3) NOT NULL,
+  `Content` mediumtext NOT NULL,
+  `DatetimePosted` datetime NOT NULL,
+  PRIMARY KEY (`ReplyID`),
+  KEY `fk_reply_CommentID_idx` (`CommentID`),
+  KEY `fk_reply_UserID_idx` (`UserID`),
+  CONSTRAINT `fk_reply_CommentID` FOREIGN KEY (`CommentID`) REFERENCES `comment` (`CommentID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_reply_UserID` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reply`
+--
+
+LOCK TABLES `reply` WRITE;
+/*!40000 ALTER TABLE `reply` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reply` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `topic`
 --
 
@@ -160,6 +267,8 @@ CREATE TABLE `user` (
   `Email` varchar(50) NOT NULL,
   `Username` varchar(30) NOT NULL,
   `Password` varchar(50) NOT NULL,
+  `Status` tinytext,
+  `Birthday` date NOT NULL,
   PRIMARY KEY (`UserID`),
   UNIQUE KEY `UserID_UNIQUE` (`UserID`),
   UNIQUE KEY `Email_UNIQUE` (`Email`),
@@ -186,4 +295,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-23  1:01:28
+-- Dump completed on 2020-06-24 11:30:48

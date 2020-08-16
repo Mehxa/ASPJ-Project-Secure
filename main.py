1730971a12ee581aa77a73babaf8bcc48252c85e
sarahndpt <sarahpishposh@gmail.com>
2020-08-16 08:18:39 +0800

    x-content headers

--- main.py
+++ main.py
@@ -124,6 +124,10 @@ def get_all_topics(option):
         topicTuples.insert(0, ('0', 'All Topics'))
     return topicTuples
 
+
+
+
+
 @app.route('/postVote', methods=["GET", "POST"])
 def postVote():
     if not sessionInfo['login']:
@@ -1099,6 +1103,12 @@ def error500(e):
     logfile.error("Error 500: Internal Server Error")
     admin = sessionInfo["isAdmin"]
     return render_template('error.html', msg=msg, admin=admin)
-
+@app.after_request
+def after_request(response):
+    response.headers.add('X-Content-Type-Options', 'nosniff')
+    response.headers["X-Frame-Options"] = "SAMEORIGIN"
+    return response
+    
 if __name__ == "__main__":
     app.run(debug=False)
+    # app.run(debug=True)

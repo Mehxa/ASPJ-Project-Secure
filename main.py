6ac3499e83aa3376547db688e583301c0fd774d4
sarahndpt <sarahpishposh@gmail.com>
2020-08-17 02:24:43 +0800

    .

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ app.config.update(
     MAIL_USE_TLS= True,
     MAIL_USE_SSL= False,
 	MAIL_USERNAME = 'deloremipsumonlinestore@outlook.com',
-	MAIL_PASSWORD = os.environ["MAIL_PASSWORD"],
+	# MAIL_PASSWORD = os.environ["MAIL_PASSWORD"],
 	MAIL_DEBUG = True,
 	MAIL_SUPPRESS_SEND = False,
     MAIL_ASCII_ATTACHMENTS = True,
